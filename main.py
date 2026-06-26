647b2dab890e879246f06260515271d603eb6536
Owen <Xiongwen.Ouyang@edu.sait.ca>
2026-06-26 05:33:13 -0600

    create class Base for database

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+
 from fastapi import FastAPI
+from sqlalchemy import DateTime, func
 from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 app = FastAPI()
 ASYNC_DATABASE_URL = "mysql+aiomysql://root:123456@localhost:3306/FastAPI_Bloom_Floral_Backend?charset=utf8"
@@ -11,6 +15,12 @@ ASYNC_DATABASE_URL,
     max_overflow= 20
 )
 
+class Base(DeclarativeBase):
+    create_time: Mapped[datetime] = mapped_column(DateTime, insert_default=func.now(), default=func.now, comment='create_time')
+    update_time: Mapped[datetime] = mapped_column(DateTime, insert_default=func.now(), default=func.now, onupdate=func.now() ,comment='update_time')
+
+class Flower(Base):
+    pass
 
 
 @app.get("/")
