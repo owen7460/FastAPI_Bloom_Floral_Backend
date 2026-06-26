a90e4371145d77001881ed3f86b16842b006ed7b
Owen <Xiongwen.Ouyang@edu.sait.ca>
2026-06-26 05:22:32 -0600

    craete async engine for mysql

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from sqlalchemy.ext.asyncio import create_async_engine
 
 app = FastAPI()
+ASYNC_DATABASE_URL = "mysql+aiomysql://root:123456@localhost:3306/FastAPI_Bloom_Floral_Backend?charset=utf8"
+
+async_engine = create_async_engine(
+ASYNC_DATABASE_URL,
+    echo = True,
+    pool_size = 10,
+    max_overflow= 20
+)
+
 
 
 @app.get("/")
